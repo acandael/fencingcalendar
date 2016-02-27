e3133af761a7d9f5b9151d33b923a0dca7ec90a8
Anthony Candaele <anthony.candaele@webmove.be>
2016-02-27 14:07:27 +0100

    fix issue with delete country

--- app/models/country.rb
+++ app/models/country.rb
@@ -1,5 +1,5 @@
 class Country < ActiveRecord::Base
-  has_many :countries
+  has_many :tournaments, :dependent => :nullify
 
   def self.options_for_select
     order('LOWER(name)').map { |e| [e.name, e.id] }
