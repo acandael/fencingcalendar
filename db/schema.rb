6904f565a967c788586b9397522e1286db971c56
Anthony Candaele <anthony.candaele@webmove.be>
2016-02-28 10:48:05 +0100

    implement start and enddate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160227131811) do
+ActiveRecord::Schema.define(version: 20160228082558) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,6 +49,7 @@ ActiveRecord::Schema.define(version: 20160227131811) do
     t.datetime "updated_at"
     t.integer  "category_id"
     t.integer  "organization_id"
+    t.date     "end_date"
   end
 
   add_index "tournaments", ["category_id"], name: "index_tournaments_on_category_id", using: :btree
